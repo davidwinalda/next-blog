ef6461bd8d8cdaf6c7209f119580e6d29b85488f
David Winalda <davidwinalda94@gmail.com>
2022-01-19 10:04:25 +0700

    update about pages

--- pages/about.js
+++ pages/about.js
@@ -5,15 +5,14 @@ import Layout from '../components/Navbar'
 export default function About() {
   return (
     <>
-      <Layout>
-        <Head>
-          <title>About Page</title>
-        </Head>
-        <div>
-          <h1>About page</h1>
-          <Image src="/images/abstract.jpeg" height={144} width={144} alt="profile image" />
-        </div>
-      </Layout>
+      <Head>
+        <title>About Page</title>
+      </Head>
+      <div>
+        <h1>About page</h1>
+        <Image src="/images/abstract.jpeg" height={144} width={144} alt="profile image" />
+        <p>Welcome to my blog site. This is demo website for building blog site with Next.js and Markdown</p>
+      </div>
     </>
   )
 }
